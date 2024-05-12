6495674b0fef233fe36a4051b7a390c49d20c86c
Vin <Vinamrasharma5@gmail.com>
2024-05-12 20:19:24 +0100

    Updated Commit
    Pushing the updated code

---
+++ src/app/(main)/page.jsx
@@ -0,0 +1,11 @@
+import { Hero } from '@/components/Hero'
+import { Newsletter } from '@/components/Newsletter'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Newsletter />
+    </>
+  )
+}
